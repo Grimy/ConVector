7eda63df74a20d0f6fcacfebba42da2cb66a63b5
natha <natha@henry.(none)>
2013-02-10 11:53:15 +0100

    ajout du fichier .ino

---
+++ drawbot.ino
@@ -0,0 +1,22 @@
+#include <SD.h>
+#include <Servo.h>
+
+#include <Drawbot.h>
+
+Draw drawbot(1000, 750, "logo1.svg");
+
+void setup() {
+  Serial.begin(57600);
+  drawbot.commencer();
+  drawbot.setVitesse(20);
+  delay(2000);
+}
+
+void loop() {
+  drawbot.svg();
+
+  drawbot.centrer();
+  drawbot.alimenter(false);
+  
+  while(true) {}
+}
