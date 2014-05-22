9aa37f4c6528a5f0e44dec220bccff201a97a642
Victor Adam <Victor.Adam@derpymail.org>
2014-05-22 21:48:08 +0200

    Coding conventions

--- src/gcodebuilder/test/GCodeCleanerTest.java
+++ src/gcodebuilder/test/GCodeCleanerTest.java
@@ -1,20 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import drawall.GCodeCleaner;
-/**
- *
- * @author natha
- */
+
 public class GCodeCleanerTest {
-    
+
     @Test
-    public void testWrongFunction()
-    {
+    public void testWrongFunction() {
         String input = "G20 X10 Y20";
         // TODO call GCodeCleaner()
         String expected = "";
@@ -23,13 +16,11 @@ public class GCodeCleanerTest {
     }
 
     @Test
-    public void testMathExpression()
-    {
+    public void testMathExpression() {
         String input = "G01 X[(10+20)*3/9] Y[5**2]";
         // TODO call GCodeCleaner()
         String expected = "G01 X10 Y25";
 
         assertEquals(input, expected);
     }
-
 }
