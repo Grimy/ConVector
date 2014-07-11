7e57d8761764884b2f95f03722640b82e1bcdc3b
Victor Adam <Victor.Adam@derpymail.org>
2014-07-11 17:42:45 +0200

    Personnal branch (unstable)

--- src/glcbuilder/src/drawall/gui/GUI.java
+++ src/glcbuilder/src/drawall/gui/GUI.java
@@ -7,23 +7,34 @@
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details. You should have received a copy of the GNU
  * General Public License along with DraWall. If not, see <http://www.gnu.org/licenses/>.
- * Copyright (c) 2012-2014 Nathanaël Jourdane.
+ * © 2012–2014 Nathanaël Jourdane
+ * © 2014 Victor Adam
  */
 
-package view;
+package drawall.gui;
 
-public class GLCBuilderGUI {
+import java.awt.Canvas;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+
+
+public class GUI extends Canvas {
+
+	// JFrame frame = new JFrame();
+	// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// frame.getContentPane().add(BorderLayout.CENTER, new GLCBuilder());
+	// frame.setSize(new Dimension(500,500));
+	// frame.setVisible(true);
 	public static void main(String... args) {
-
+		assert false;
 	}
-}
 
-/* modules parameters:
- * name, type, min, max, values
- *
- * int -> spinner
- * int with min and max -> cursor
- * int with values -> listbox
- * String -> textbox
- * boolean -> checkbox
- */
+	@Override
+	public void paint(Graphics graphics) {
+		super.paint(graphics);
+		System.out.println(graphics);
+		Graphics2D g = (Graphics2D) graphics;
+		pickPlugin().process(input, g);
+	}
+
+}
