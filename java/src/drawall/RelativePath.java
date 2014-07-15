f869073239bc3f4859b02924e2f7f1da67c58951
Victor Adam <Victor.Adam@derpymail.org>
2014-07-15 18:43:00 +0200

    Added missing RelativePath file

---
+++ java/src/drawall/RelativePath.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of DraWall.
+ * DraWall is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * DraWall is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with DraWall. If not, see <http://www.gnu.org/licenses/>.
+ * © 2012–2014 Nathanaël Jourdane
+ * © 2014 Victor Adam
+ */
+
+package drawall;
+
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+
+/** Adds method for relative movement to Sun's Path. */
+public class RelativePath extends Path2D.Double {
+
+	public void rLineTo(double x, double y) {
+		Point2D p = getCurrentPoint();
+		System.out.println("lineto: " + (p.getX() + x) + ", " + (p.getY() + y));
+		lineTo(p.getX() + x, p.getY() + y);
+	}
+}
