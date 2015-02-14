990568bd458d75131787e65149800ac5318e2ad4
Nathanaël Jourdane <roipoussiere@gmail.com>
2015-02-14 15:34:11 +0100

    GCode: plotter-compliant output

--- src/cc/drawall/gcode/GCodeExporter.java
+++ src/cc/drawall/gcode/GCodeExporter.java
@@ -20,7 +20,7 @@ public class GCodeExporter extends Exporter {
 
 	/** Constructor. */
 	public GCodeExporter() {
-		super(MERGE | SHORTEN | REVERSE, "G00 X% Y%", "G01 X% Y%", "G5.1 I% J% X% Y%", "G5 I% J% P% Q% X% Y%");
+		super(MERGE | SHORTEN | FLATTEN | REVERSE, "G00 X% Y%", "G01 X% Y%");
 	}
 
 	@Override
