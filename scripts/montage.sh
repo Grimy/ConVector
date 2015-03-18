a4d26c3848f48615b09eb8dc89b7851e95d379bf
Victor Adam <victor@drawall.cc>
2015-03-18 02:42:51 +0100

    Tests: fixes
    * Correct the path to the jar (was wrong since montage.sh was moved)
    * Move plant.ps to examples (it uses rand and thus cannot be tested)
    * Use -metric MSE as it gives more accurate results than -metric AE
    * Disable antialiasing
    * Increase the tolerance from 100 to 256
    * Reduce the order on the fractals to save time
    * Minor positionning adjustments

--- scripts/montage.sh
+++ scripts/montage.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 export MAGICK_THREAD_LIMIT=1 MAGICK_MEMORY_LIMIT=256MiB
-ROOT="$(dirname $(readlink -f "$0"))"
+ROOT="$(dirname $(dirname $(readlink -f "$0")))"
 cd "$(dirname $(readlink -f "$1"))"
 FILE="$(basename "$1")"
 mkdir -p pdf ps miff diff montage svg
 java -ea -jar "$ROOT/convector.jar" "$FILE" "svg/$FILE.svg" "ps/$FILE.ps" "pdf/$FILE.pdf"
-convert -antialias "$FILE" "miff/$FILE.miff"
+convert +antialias "$FILE" "miff/$FILE.miff"
 ALL="miff/$FILE.miff svg/$FILE.svg diff/$FILE.miff"
-SCORE="$(compare -alpha off -metric MAE $ALL 2>&1 | cut -d' ' -f1 | cut -d. -f1)"
+SCORE="$(compare -alpha off -metric MSE $ALL 2>&1 | cut -d' ' -f1 | cut -d. -f1)"
 printf "%5d %s\n" "$SCORE" "$FILE"
-[ "$SCORE" -lt 100 ] || ! \
-montage -background white $ALL "ps/$FILE.ps" "pdf/$FILE.pdf" -geometry 533x450 "montage/$FILE.miff"
+[ "$SCORE" -lt 256 ] || ! \
+montage -background white $ALL "ps/$FILE.ps" "pdf/$FILE.pdf" -geometry 533x450 "montage/$FILE.png"
